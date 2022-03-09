a20f39a5ed2124e139adcf8be5759b0271166b74
bilkn <bilkan.konus@gmail.com>
2022-03-09 23:33:29 +0300

    feat: add Hamburger Menu

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import { Header, Introduction } from "../components";
-import { useTheme } from "@emotion/react";
 
 export default function Index() {
   return (
     <>
+      <Header />
       <Container
         sx={{
-          minHeight: "100%",
           padding: "0",
         }}
       >
-        <Header />
         <Introduction />
       </Container>
     </>
