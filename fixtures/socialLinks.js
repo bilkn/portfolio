dfb262baf89b9ab8ae60f5d4058e0aa906e44fb8
bilkn <bilkan.konus@gmail.com>
2022-03-27 17:46:34 +0300

    feat: improve accessibility

--- fixtures/socialLinks.js
+++ fixtures/socialLinks.js
@@ -1,7 +1,11 @@
 import { GitHub, LinkedIn, Mail } from "@mui/icons-material";
 
 export const socialLinks = [
-  { icon: <LinkedIn />, to: "https://www.linkedin.com/in/bilkankonus/" },
-  { icon: <GitHub />, to: "https://github.com/bilkn" },
-  { icon: <Mail />, to: "mailto:bilkan.konus@gmail.com" },
+  {
+    icon: <LinkedIn />,
+    to: "https://www.linkedin.com/in/bilkankonus/",
+    name: "Linkedin",
+  },
+  { icon: <GitHub />, to: "https://github.com/bilkn", name: "GitHub" },
+  { icon: <Mail />, to: "mailto:bilkan.konus@gmail.com", name: "Email" },
 ];
