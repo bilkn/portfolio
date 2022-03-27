b3bd7b24885d2b8a5e4dc16da76f064f25fdfb0b
bilkn <bilkan.konus@gmail.com>
2022-03-27 16:41:23 +0300

    feat: add more animations

--- components/Introduction/Introduction.jsx
+++ components/Introduction/Introduction.jsx
@@ -1,8 +1,11 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Slide, Typography } from "@mui/material";
 import React from "react";
 import { Section } from "..";
+import useShowItemOnIntersect from "../../hooks/useShowItemOnIntersect";
 
 function Introduction() {
+  const { showItem, ref } = useShowItemOnIntersect();
+
   return (
     <Section id="about">
       <Box
@@ -13,22 +16,28 @@ function Introduction() {
           display: "flex",
         }}
       >
-        <Box sx={{ flexBasis: "55%" }}>
-          <Typography variant="h1" sx={{ mb: { xs: 2, md: 3 } }}>
-            Hello I am Bilkan, <br />a Fullstack Developer
-          </Typography>
-          <Typography variant="body1" sx={{ lineHeight: { xs: 1.4, md: 1.6 } }}>
-            I have been building personal/professional projects for four years.
-            I can build scalable web/mobile applications in a very short time.
-          </Typography>
-          <Button sx={{ mt: 6, display: { xs: "none", md: "block" } }}>
-            Contact
-          </Button>
-        </Box>
+        <Slide direction="right" ref={ref} in={showItem} timeout={1000}>
+          <Box sx={{ flexBasis: "55%" }}>
+            <Typography variant="h1" sx={{ mb: { xs: 2, md: 3 } }}>
+              Hello I am Bilkan, <br />a Fullstack Developer
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{ lineHeight: { xs: 1.4, md: 1.6 } }}
+            >
+              I have been building personal/professional projects for four
+              years. I can build scalable web/mobile applications in a very
+              short time.
+            </Typography>
+            <Button sx={{ mt: 6, display: { xs: "none", md: "block" } }}>
+              Contact
+            </Button>
+          </Box>
+        </Slide>
         <Box
           sx={{
             display: "flex",
-            justifyContent:'center',
+            justifyContent: "center",
             flexBasis: "45%",
             mt: { xs: 6, md: "0" },
             ml: { xs: "0", md: 3 },
@@ -40,13 +49,13 @@ function Introduction() {
             camera-controls
             auto-rotate
             src="/images/3d-avatar.glb"
-            post='/images/avatar.png'
+            post="/images/avatar.png"
             alt="Bilkan Konus"
-            camera-orbit="45deg 55deg 100m"
+            camera-orbit="25deg 85deg 100m"
             style={{ height: "400px" }}
           ></model-viewer>
         </Box>
-        <Button sx={{ mt:6, display: { xs: "block", md: "none" } }}>
+        <Button sx={{ mt: 6, display: { xs: "block", md: "none" } }}>
           Contact
         </Button>
       </Box>
--- components/SocialLinks/SocialLinks.jsx
+++ components/SocialLinks/SocialLinks.jsx
@@ -1,50 +1,55 @@
 import { useTheme } from "@emotion/react";
-import { Box, Link, List, Stack } from "@mui/material";
+import { Box, Link, List, Slide, Stack } from "@mui/material";
 import React from "react";
 import { socialLinks } from "../../fixtures/socialLinks";
+import { useShowItemOnIntersect } from "../../hooks";
 
 function SocialLinks() {
   const theme = useTheme();
+  const { showItem, ref } = useShowItemOnIntersect();
 
   return (
-    <Box
-      sx={{
-        background:
-          "radial-gradient(198.75% 176.52% at -48.75% -18.56%, rgba(255, 255, 255, 0.49) 0%, rgba(238, 238, 238, 0.21) 100%)",
-        borderTopRightRadius: "5px",
-        borderBottomRightRadius: "5px",
-        display: { xs: "none", md: "block" },
-        top: "50%",
-        transform: "translateY(-50%)",
-        position: "fixed",
-        zIndex: theme.zIndex.overlayUI,
-      }}
-    >
-      <List>
-        <Stack>
-          {socialLinks.map(({ icon, to }, i) => (
-            <>
-              <Link
-                key={to}
-                href={to}
-                sx={{
-                  borderBottom: "1px solid rgba(0, 0, 0, 0.06)",
-                  color: "text.secondary",
-                  p: 0,
-                  transition: "color 150ms",
-                  "&:hover": {
-                    color: "text.primary",
-                  },
-                }}
-                target="_blank"
-              >
-                {React.cloneElement(icon, { sx: { fontSize: "3.5rem" } })}
-              </Link>
-            </>
-          ))}
-        </Stack>
-      </List>
-    </Box>
+    <Slide direction="right" in={showItem} timeout={500}>
+      <Box
+        ref={ref}
+        sx={{
+          background:
+            "radial-gradient(198.75% 176.52% at -48.75% -18.56%, rgba(255, 255, 255, 0.49) 0%, rgba(238, 238, 238, 0.21) 100%)",
+          borderTopRightRadius: "5px",
+          borderBottomRightRadius: "5px",
+          display: { xs: "none", md: "block" },
+          top: "50%",
+          transform: "translateY(-50%)",
+          position: "fixed",
+          zIndex: theme.zIndex.overlayUI,
+        }}
+      >
+        <List>
+          <Stack>
+            {socialLinks.map(({ icon, to }, i) => (
+              <>
+                <Link
+                  key={to}
+                  href={to}
+                  sx={{
+                    borderBottom: "1px solid rgba(0, 0, 0, 0.06)",
+                    color: "text.secondary",
+                    p: 0,
+                    transition: "color 150ms",
+                    "&:hover": {
+                      color: "text.primary",
+                    },
+                  }}
+                  target="_blank"
+                >
+                  {React.cloneElement(icon, { sx: { fontSize: "3.5rem" } })}
+                </Link>
+              </>
+            ))}
+          </Stack>
+        </List>
+      </Box>
+    </Slide>
   );
 }
 
